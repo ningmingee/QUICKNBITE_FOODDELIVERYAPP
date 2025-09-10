4aa8657b15e0c22450d73459fc1c89aac1e85844
mg <ningmg-pm24@student.tarc.edu.my>
2025-09-10 23:04:33 +0800

    Delete app/quicknbite-combined directory

--- app/quicknbite-combined/app/src/main/java/com/example/quicknbiteapp/data/model/OrderStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quicknbiteapp.data.model
-
-enum class OrderStatus {
-    PENDING,
-    PREPARING,
-    READY_FOR_PICKUP,
-    COMPLETED,
-    CANCELLED
-}
--- app/quicknbite-combined/app/src/main/java/com/example/quicknbiteapp/data/model/OrderType.kt
+++
@@ -1,5 +0,0 @@
-package com.example.quicknbiteapp.data.model
-
-enum class OrderType {
-    DELIVERY
-}
